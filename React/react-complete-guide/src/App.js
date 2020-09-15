c0cb824cc8d4d1e24e59bba1e7359a577aeb6b47
Federico Parezzan <parez93@users.noreply.github.com>
2020-09-15 15:59:02 +0200

    [REACT] hooks

--- React/react-complete-guide/src/App.js
+++ React/react-complete-guide/src/App.js
@@ -1,28 +1,82 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Person from './Person/Person';
 
-class App extends Component {
-
-  state = {
+const app = props => {
+  const [personState, setPersonsState] = useState({
     persons: [
       {name: 'Fede', age: 29},
       {name: 'Fede2', age: 28},
       {name: 'Fede3', age: 27}
-    ]
+    ],
+  });
+
+  /*
+  VERSIONE PULITA
+  E' buona norma non creare un unico grande oggetto che gestisce lo stato, ma più sezioni di stato separate
+  */
+  const [otherState, setOtherState] = useState("some other value");
+
+  console.log(personState, otherState);
+
+  const switchNameHandler = (newName) => {
+    console.log("Was clicked!");
+    // DON'T DO THIS: this.state.persons[0].name = "Federico";
+    setPersonsState({
+      persons: [
+        {name: newName, age: 290},
+        {name: 'Fede2Change', age: 280},
+        {name: 'Fede3Change', age: 270}
+      ],
+      /*
+      VERSIONE POCO ELEGANTE
+      // lo devo aggiungere perchè con gli hook non viene fatto una copia del precedente valore
+      otherState: personState.otherState 
+      */
+   });
   }
 
-  render() {
+nameChangeHandler = (event) => {
+  this.setPersonsState({
+    persons: [
+      {name: event.target.value, age: 290},
+      {name: 'Fede2Change', age: 280},
+      {name: 'Fede3Change', age: 270}
+    ],
+ });
+}
+
+const style = {
+  backgroundColor: 'white',
+  font: 'inherit',
+  border: '1px solid blue',
+  padding: '8px',
+  cursor: 'pointer'
+};
+
     return (
       <div className="App">
         <h1>Ciao!</h1>
-        <button>Switch Name</button>
-        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}>1 person</Person>
-        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>2 person</Person>
+        <!-- meglio usare questo se possibile-->
+        <button 
+          style={style}
+          onClick={this.switchNameHandler.bind(this, "Federico")}>Switch Name</button>
+        <!-- puo essere inefficiente -->
+        <button onClick={() => this.switchNameHandler("Federico!")}>Switch Name 2</button> 
+        <Person 
+          name={personState.persons[0].name} 
+          age={personState.persons[0].age}
+          click={this.switchNameHandler.bind(this, "Federi")}
+          changed={this.nameChangeHandler}>1 person</Person>
+        <Person 
+          name={personState.persons[1].name} 
+          age={personState.persons[1].age}
+          click={this.switchNameHandler.bind(this, "Fed")}>2 person</Person>
       </div>
     );
-  }
 }
 
-export default App;
+export default app;
+
+
--- React/react-complete-guide/src/Person/Person.js
+++ React/react-complete-guide/src/Person/Person.js
@@ -1,11 +1,13 @@
 import React from 'react';
+import "./Person.css";
 
 const person = (props) => {
     return (
-        <div>
-            <p>I'm a Person! and I am {Math.floor(Math.random() * 30 )} years old</p>
+        <div className="Person">
+            <p onClick={props.click}>I'm a Person! and I am {Math.floor(Math.random() * 30 )} years old</p>
             <p>I'm {props.name}! and I am {props.age} years old</p>
             <p>{props.children}</p>
+            <input type="text" onChange={props.changed} value="props.name"/>
         </div>
     );
 };
